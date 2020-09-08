f599988b3dce1f9825748a9d73121ea9612b9849
Mehmet Tezel <56451989+TzLcorp@users.noreply.github.com>
2020-09-08 23:26:36 +0300

    Update Injector.cpp

--- NEO INJECT/EN/src/TH/Injector.cpp
+++ NEO INJECT/EN/src/TH/Injector.cpp
@@ -1,3 +1,11 @@
+/*
+    This software is owned by Mehmet Tezel.
+    You can change and use as you wish.
+    (Provided that the source is specified)
+    https://github.com/TzLcorp
+    TezeL Corporation
+*/
+
 #include <Windows.h>
 #include <iostream>
 #include <fstream>
